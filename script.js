712db2ebe5c81fc07d30a545de92aec30dbd40bd
Don <don.horde@gmail.com>
2024-03-13 21:24:08 +0100

    add basic console functionality, remove excess boilerplate

--- script.js
+++ script.js
@@ -1,2 +1,46 @@
-let success = "External script loaded correctly"
-console.log(success);
+const getComputerChoice = () => {
+    const value = Math.floor(Math.random() * 3);
+    let result = "";
+    switch (value) {
+        case 0:
+            result = "rock";
+            break;
+        case 1:
+            result = "paper";
+            break;
+        case 2:
+            result = "scissors";
+            break;
+        default:
+            throw new Error("Something has gone wrong generating the computer's result.");
+    }
+    return result;
+}
+
+function playRound(playerSelection, computerSelection){
+    let result = "";
+    let userInput = prompt("Rock, Paper, Scissors?");
+    playerSelection = userInput.toLowerCase();
+    computerSelection = getComputerChoice();
+    const announce = x => {
+        if (x === "draw") {
+            console.log(`It's a draw! You both chose ${playerSelection}`);
+        } else if (x === "lose") {
+            console.log(`You lose! You chose ${playerSelection}, PC chose ${computerSelection}`);
+        } else if (x === "win") {
+            console.log(`You win! You chose ${playerSelection}, PC chose ${computerSelection}`);
+        } else {
+            console.log("You must choose paper, rock, or scissors.");
+        }
+    }
+    if (playerSelection === computerSelection) {announce("draw");}
+    else if  (playerSelection === "paper" && computerSelection === "rock" ||
+              playerSelection === "rock" && computerSelection === "scissors" ||
+              playerSelection === "scissors" && computerSelection === "paper") {announce("win");}
+    else if  (playerSelection === "rock" && computerSelection === "paper" ||
+              playerSelection === "scissors" && computerSelection === "rock" ||
+              playerSelection === "paper" && computerSelection === "scissors") {announce("lose");}
+    else {announce()};
+}
+
+playRound();
